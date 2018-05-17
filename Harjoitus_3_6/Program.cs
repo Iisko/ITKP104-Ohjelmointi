99b1a012e16974deb9cc3bf4b410d375f90d9654
Iisakki Säkkinen <einoiisko@gmail.com>
2018-05-17 22:21:07 +0300

    Harjoitus_3_6 - Laitettu alkuun, jäi kesken vielä.

--- Harjoitus_3_6/Program.cs
+++ Harjoitus_3_6/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
 
 namespace Harjoitus_3_6
 {
@@ -9,6 +12,36 @@ namespace Harjoitus_3_6
     {
         static void Main(string[] args)
         {
+            Socket s = null;
+
+            int port = 9999;
+            Console.WriteLine("Käytössä oleva portti: {0}", port);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Loopback, port);
+
+            List<EndPoint> asiakkaat = new List<EndPoint>();
+
+            try
+            {
+                s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                s.Bind(iep);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Virhe: " + ex.Message);
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Odotetaan asiakasta...");
+
+            while(!Console.KeyAvailable)
+            {
+                s.ReceiveFrom();
+            }
+
+            Console.ReadKey();
+            s.Close();
+
         }
     }
 }
