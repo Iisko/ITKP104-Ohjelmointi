0ed99c06298058322395f5c4a64b0aa7d0e22eac
Iisakki Säkkinen <einoiisko@gmail.com>
2018-05-18 19:09:29 +0300

    Harjoitus_4_8 valmis!

--- Harjoitus_4_8/Program.cs
+++ Harjoitus_4_8/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
 
 namespace Harjoitus_4_8
 {
@@ -9,6 +12,57 @@ namespace Harjoitus_4_8
     {
         static void Main(string[] args)
         {
+            Socket palvelin = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint iep = new IPEndPoint(IPAddress.Loopback, 9999);
+            EndPoint ep = (EndPoint)iep;
+            EndPoint Pep = (IPEndPoint)ep;
+
+            palvelin.SendTo(Encoding.ASCII.GetBytes("JOIN Iisakin asiakas"), Pep);
+            bool on = true;
+            String TILA = "JOIN";
+            byte[] data = new byte[256];
+            while(on)
+            {
+                palvelin.Receive(data);
+                String vastaus = Encoding.ASCII.GetString(data);
+                String[] palat = vastaus.Split(' ');
+                switch(TILA)
+                {
+                    case "JOIN":
+                        switch (palat[0])
+                        {
+                            case "ACK":
+                                switch (palat[1])
+                                {
+                                    case "201":
+                                        Console.WriteLine("Odotetaan toista pelaajaa...");
+                                        break;
+                                    case "202":
+                                        Console.WriteLine("Vastustajasi on {0}.", palat[2]);
+                                        Console.WriteLine("Anna numero");
+                                        String luku = Console.ReadLine();
+                                        palvelin.SendTo(Encoding.ASCII.GetBytes("DATA " + luku), Pep);
+                                        TILA = "GAME";
+                                        break;
+                                    case "203":
+                                        Console.WriteLine("Vastustaja {0} saa aloittaa.", palat[2]);
+                                        TILA = "GAME";
+                                        break;
+                                    default:
+                                        //Console.WriteLine("Virhe " + palat[1]);
+                                        break;
+                                }
+                                break;
+                            default:
+                                Console.WriteLine("Virhe " + palat[0]);
+                                break;
+                        }
+                        break;
+                    case "GAME":
+                        break;
+                }
+            }
         }
     }
 }
